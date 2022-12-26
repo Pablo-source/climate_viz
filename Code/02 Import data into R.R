55e82ea6c92bf926d0b3f8f8488e49e629e2c9c3
Pablo Leon <datavizvalencia@gmail.com>
2022-12-26 17:11:48 +0000

    Including script to import data and produce initial exploratory line chart

--- Code/02 Import data into R.R
+++ Code/02 Import data into R.R
@@ -1,7 +1,14 @@
 ## 02 Import csv data into R.R
 
+# Read .csv data file into R using read_csv() function from readr package
+# Part of tidyverse universe
 library(tidyverse)
 
 # Read in data using read_csv() function from readr library
 
-Data <- read_csv()
+
+Data <- read_csv("~/Documents/Pablo_Desktop/Github_Pablo_Source_Local_copy/climate_viz/data/Global_temp.csv",
+                 skip = 1, na = "***") %>% 
+        select(Year, 'J-D') %>% 
+        select(year = Year, t_diff = 'J-D')
+Data
